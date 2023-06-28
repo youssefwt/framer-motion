8a6fa5f1b2b8d3bb91cc6acc59e3105b82f19dc8
youssefw <ywtalaat@gmail.com>
2023-06-28 22:19:35 +0300

    the "initial" prop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,14 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
     return (
-        <motion.div className="home container" >
-            <motion.h2 animate={{ fontSize: "50px", color: "#ff2994", }}>
+        <motion.div className="home container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}>
+            <motion.h2 animate={{}}>
                 Welcome to Pizza Joint
             </motion.h2>
             <Link to="/base">
-                <motion.button animate={{ scale: 1.2 }}>
+                <motion.button animate={{}}>
                     Create Your Pizza
                 </motion.button>
             </Link>
